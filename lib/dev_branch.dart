62be521a32e7e2624735119514f2ef719f09f9f4
Mohamed AbdelQader <mohamedabdelqadersaeed@gmail.com>
2024-10-28 16:15:50 +0300

    update changes

---
+++ lib/dev_branch.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class dev_branch extends StatelessWidget {
+  const dev_branch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("dev branch"),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  Container(child:Text( 'Flutter Demo Home Page')),
+      home:  Container(child:Text( 'Dev branch')),
     );
   }
 }
